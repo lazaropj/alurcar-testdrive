b5ba165c1120b8d14388dd8fa9a57cdc625644ef
Lazaro Jr <lazaro@bitbank.com>
2016-08-25 16:33:32 -0300

    Aula 1

---
+++ Aula-1/AluraCar/www/js/routes.js
@@ -0,0 +1,29 @@
+angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/login');
+
+  $stateProvider
+
+  .state('listagem', {
+    url: '/listagem',
+    templateUrl: 'templates/listagem.html',
+    controller: 'ListagemController'
+  })
+
+  .state('carroescolhido', {
+    url: '/carroescolhido/:carro',
+    templateUrl: 'templates/carroescolhido.html',
+    controller: 'CarroEscolhidoController'
+  })
+
+  .state('finalizarpedido', {
+    url: '/finalizarpedido/:carro',
+    templateUrl: 'templates/finalizarpedido.html',
+    controller: 'FinalizarPedidoedidoController'
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'LoginController'
+  })
+});
